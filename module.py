8ed4a9d64e8247bd1a81fd40ce074686e99bc70a
robopugo <reimacdougall@gmail.com>
2026-06-18 15:03:42 -0500

    added main utility funcs

--- module.py
+++ module.py
@@ -9,6 +9,7 @@ def clear(n=""):
     return n    
 
 def leadZero (i, d):
+    """number, digits. (1,3) -> '001'"""
     i = str(i)
     i = "0" * (d - len(i)) + i
     return i
@@ -73,38 +74,143 @@ def Demo256():
             print(end="\t")
         print(end=" " if i < 15 else "")
 
-def rgb(r,g,b,m):
-    return f"\033[38;2;{r};{g};{b}m" if m.lower() == "f" else "\033[48;2;{r};{g};{b}m" if m.lower() == "b" else ""
+def rgb(r,g,b,m="f"):
+    """0 to 255 for each color, foreground/background"""
+    return f"\033[38;2;{r};{g};{b}m" if m.lower()[0] == "f" else "\033[48;2;{r};{g};{b}m" if m.lower() == "b" else ""
 
 class cursor:
-    def invis():
-        print("\033[?25l")
+    """invis() and vis() may not cetain terminals."""
+    def invis(): 
+        return ("\033[?25l")
     def vis():
-        print("\033[?25h")
+        return ("\033[?25h")
     def up(n=1):
-        print(f"\033[{n}A")
+        return (f"\033[{n}A")
     def down(n=1):
-        print(f"\033[{n}B")
+        return (f"\033[{n}B")
     def left(n=1):
-        print(f"\033[{n}C")
+        return (f"\033[{n}D")
     def right(n=1):
-        print(f"\033[{n}D")
+        return (f"\033[{n}C")
     def nextLine(n=1):
-        print(f"\033[{n}E")
+        return (f"\033[{n}E")
     def prevLine(n=1):
-        print(f"\033[{n}F")
-    def colum(n):
-        print(f"\033[{n}G")
+        return (f"\033[{n}F")
+    def collum(n):
+        return (f"\033[{n}G")
+    def getPos():
+        return ("\033[6n")
+    def up1():
+        return ("\033 M")
     def setPos(x="",y=""):
-        print(f"\033[{y};{x}H")
+        return (f"\033[{y};{x}H")
     def savePos():
-        print("\033[s")
+        return ("\033[s")
     def loadPos():
-        print("\033[u")
+        return ("\033[u")
     def saveAll():
-        print("\0337")
+        return ("\0337")
     def loadAll():
-        print("\0338")
+        return ("\0338")
+
+chars = {
+    """custom chars"""
+    "BEL" : "\a",    # terminal bell
+    "BS"  : "\b",    # backspace
+    "HT"  : "\t",    # horizontal tab
+    "LF"  : "\n",    # linefeed (newline)
+    "VT"  : "\v",    # vertical tab
+    "FF"  : "\f",    # formfeed (also: new page NP)
+    "CR"  : "\r",    # carriage return
+    "ESC" : "\x1B", # escape charater
+    "DEL" : "\x7F"  # delete charater
+}
+
+
+    # dosent move cursor
+
+class screen:
+    class erase:
+        def CtoEnd():
+            return "\0330J"
+        def CtoStart():
+            return "\0331J"
+        def all():
+            return "\0332J"
+        def saved():
+            return "\0333J"
+    def save():
+        return "\033[?47h"
+    def load():
+        return "\033[?47l"
+
+class line:
+    class erase:
+        def CtoEnd():
+            return "\0330K"
+        def CtoStart():
+            return "\0331K"
+        def all():
+            return "\0332K"
+
+class graphics:
+    """text decorators, add/remove"""
+    add = {
+        "none"          : "\033[0m",
+        "bold"          : "\033[1m",
+        "dim"           : "\033[2m",
+        "italic"        : "\033[3m",
+        "underline"     : "\033[4m",
+        "Blink"         : "\033[5m",
+        "Reverse"       : "\033[7m",
+        "hidden"        : "\033[8m",
+        "strikethrough" : "\033[9m"}
+    remove = {
+        "bold"          : "\033[22m",
+        "dim"           : "\033[22m",
+        "italic"        : "\033[23m",
+        "underline"     : "\033[24m",
+        "Blink"         : "\033[25m",
+        "Reverse"       : "\033[27m",
+        "hidden"        : "\033[28m",
+        "strikethrough" : "\033[29m"}
+
+def color(name="default", m="f", bright=False):
+    """name, foreground/background (f,b), bright (true/false)"""
+    names = ["black","red","green","yellow","blue","magenta","cyan","white",None,"default"]
+    return f"\033[{names.index(name.lower()) + 30 + (10 if m.lower()[0] == 'b' else 0) + (60 if bright else 0)}m" if name else ""
+    
+def color256(id, m="f"):
+    """
+    0-7: standard colors (as in ESC [ 30-37 m)
+    8-15: high intensity colors (as in ESC [ 90-97 m)
+    16-231: 6 * 6 * 6 cube (216 colors): 16 + 36 * r + 6 * g + b (0 ≤ r, g, b ≤ 5)
+    232-255: grayscale from dark to light in 24 steps.
+    """
+    return f"\033[{38 if m.lower()[0] == 'f' else 48};5;{id}m"
+
+def setMode(id, m="add"):
+    """0 <= id <= 7 or 13 <= id <= 19 , add/remove (a/r)
+    Changes the screen width or type to the mode specified by id.
+    0 - 40 x 25 monochrome (text)
+    1 - 40 x 25 color (text)
+    2 - 80 x 25 monochrome (text)
+    3 - 80 x 25 color (text)
+    4 - 320 x 200 4-color (graphics)
+    5 - 320 x 200 monochrome (graphics)
+    6 - 640 x 200 monochrome (graphics)
+    7 - Enables line wrapping
+
+    13 - 320 x 200 color (graphics)
+    14 - 640 x 200 color (16-color graphics)
+    15 - 640 x 350 monochrome (2-color graphics)
+    16 - 640 x 350 color (16-color graphics)
+    17 - 640 x 480 monochrome (2-color graphics)
+    18 - 640 x 480 color (16-color graphics)
+    19 - 320 x 200 color (256-color graphics)
+    1049 - alternative buffer
+    """
+    return f"\033[{'=' if id != 1049 else '?'}{id}{'h' if m.lower()[0] == 'a' else 'l'}"
 
 def main():
     global bg_color
@@ -113,5 +219,5 @@ def main():
     print("i like green")
 
 if __name__ == "__main__":
-    main()
+    tableDemo()
     clear(input("clear: "))
